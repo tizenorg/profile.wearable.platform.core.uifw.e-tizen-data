2d50e551515815d899226392a50e4a648dffcf30
Boram Park <boram1288.park@samsung.com>
2015-08-31 17:23:53 +0900

    add enlightenment-user service for app to access "/run/wayland-0" file
    Change-Id: I3ab9cbc8593db67ed7d2eb34fd99e540ea89c4ce

--- default/wayland/enlightenment.sh
+++ default/wayland/enlightenment.sh
@@ -1,9 +1,5 @@
-#export ELM_ENGINE=wayland_egl
-#export ECORE_EVAS_ENGINE=wayland_egl
-#export ELM_THEME=tizen-HD-light
-
-# Make EFL apps use the wayland-based input method.
-#export ECORE_IMF_MODULE=wayland
-
-# workaround systemd bug in pam_systemd module
-export XDG_RUNTIME_DIR=/run
+if [ "$USER" == "root" ]; then
+	export XDG_RUNTIME_DIR=/run
+else
+	export XDG_RUNTIME_DIR=/run/user/$UID
+fi
